06f5e5fc98150897e9da3cef8147a7d35beeb28a
nilesh-chavan-07 <nileshchavan07@yahoo.com>
2025-06-05 15:52:18 +0530

    secondLargestNumber

--- src/main/java/com/tka/Operation.java
+++ src/main/java/com/tka/Operation.java
@@ -408,6 +408,26 @@ public class Operation {
 	}
 
 	public static void secondLargest() {
+		
+		 int[] numbers = {12, 35, 1, 10, 34, 1};
+		 
+		  int largest = Integer.MIN_VALUE;
+	        int secondLargest = Integer.MIN_VALUE;
+
+	        for (int num : numbers) {
+	            if (num > largest) {
+	                secondLargest = largest;
+	                largest = num;
+	            } else if (num > secondLargest && num != largest) {
+	                secondLargest = num;
+	            }
+	        }
+
+	        if (secondLargest == Integer.MIN_VALUE) {
+	            System.out.println("There is no second largest number.");
+	        } else {
+	            System.out.println("The second largest number is: " + secondLargest);
+	        }
 	}
 
 	public static void compareArrays() {
